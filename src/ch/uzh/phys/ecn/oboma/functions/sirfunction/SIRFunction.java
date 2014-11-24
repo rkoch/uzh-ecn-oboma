65af1503a85786080298fb7fe92dae47ef2ec724
wetret <retowettstein@hotmail.com>
2014-11-24 18:30:06 +0100

    Add SIR calculation to SIRFunction$

--- src/ch/uzh/phys/ecn/oboma/functions/sirfunction/SIRFunction.java
+++ src/ch/uzh/phys/ecn/oboma/functions/sirfunction/SIRFunction.java
@@ -11,11 +11,37 @@ public class SIRFunction implements ITransformationFunction{
     @Override
     public InfectionState apply(Agent pAgent, INode pNode) {
 
+        double[] sirInNode = getSIRInNode(pNode);
+        double[] newDiseaseDistributionInNode = calculateSIR(sirInNode[0], sirInNode[1], sirInNode[2]);
+        InfectionState newAgentInfectionState = getNewInfectionState(newDiseaseDistributionInNode);
+
+        return newAgentInfectionState;
+    }
+
+    private double[] getSIRInNode(INode pNode){
+        //TODO: extract S, I and R from pNode
+
         return null;
     }
 
-    private void calculateSIR(double pSusceptible, double pInfected, double pRecovered){
-        //See mainForEuler class from ECN1
+    private double[] calculateSIR(double pSusceptible, double pInfected, double pRecovered){
+        ODESIR sir = new ODESIR();
+        ODERK2 rk2 = new ODERK2();
+
+        double dt = 1;
+        double tn = 1;
+        double[] yn = {pSusceptible, pInfected, pRecovered};
+        double[] yn1 = new double[3];
+
+        yn1 = rk2.step(yn, tn, dt, sir);
+
+        return yn1;
+    }
+
+    private InfectionState getNewInfectionState(double[] pInfectionDistribution){
+        //TODO calculate new InfectionState of agent
+
+        return null;
     }
 
 }
