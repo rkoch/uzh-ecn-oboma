4e79bfa341645b1ba8c6ea347f09f231121328a1
wetret <retowettstein@hotmail.com>
2014-11-24 14:08:24 +0100

    Add Recovery Rate

--- src/ch/uzh/phys/ecn/oboma/common/DiseaseConstants.java
+++ src/ch/uzh/phys/ecn/oboma/common/DiseaseConstants.java
@@ -3,8 +3,10 @@ package ch.uzh.phys.ecn.oboma.common;
 
 public class DiseaseConstants {
 
-    public static final double TRAIN_INFECTTION_RATE = 0.0;
+    public static final double TRAIN_INFECTION_RATE = 0.0;
     public static final double CAR_INFECTION_RATE    = 0.0;
     public static final double CITY_INFECTION_RATE   = 0.0;
+
+    public static final double RECOVERY_RATE = 0.0;
 
 }
