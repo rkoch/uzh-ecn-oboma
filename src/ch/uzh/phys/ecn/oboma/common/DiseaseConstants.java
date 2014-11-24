6a5f0d26d9a7a2c461ef13e267abc8ce75c70d40
Reto Wettstein <retwet@dhcp-wlan-uzh-89-206-84-229.uzh.ch>
2014-11-24 14:03:22 +0100

    Add some Infection Rates of Train, Car and City

--- src/ch/uzh/phys/ecn/oboma/common/DiseaseConstants.java
+++ src/ch/uzh/phys/ecn/oboma/common/DiseaseConstants.java
@@ -3,4 +3,8 @@ package ch.uzh.phys.ecn.oboma.common;
 
 public class DiseaseConstants {
 
+    public static final double TRAIN_INFECTTION_RATE = 0.0;
+    public static final double CAR_INFECTION_RATE    = 0.0;
+    public static final double CITY_INFECTION_RATE   = 0.0;
+
 }
--- src/ch/uzh/phys/ecn/oboma/functions/api/TransformationFunction.java
+++ src/ch/uzh/phys/ecn/oboma/functions/api/TransformationFunction.java
@@ -3,11 +3,11 @@ package ch.uzh.phys.ecn.oboma.functions.api;
 import ch.uzh.phys.ecn.oboma.agents.api.Agent;
 import ch.uzh.phys.ecn.oboma.map.api.INode;
 
+
 public class TransformationFunction implements ITransformationFunction{
 
     @Override
     public void apply(Agent pAgent, INode pNode) {
-        // TODO Auto-generated method stub
 
     }
 
