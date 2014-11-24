c21c335a4dc7a8997c17c2f3ca0de12b2e53f82c
wetret <retowettstein@hotmail.com>
2014-11-24 14:19:51 +0100

    Add Runge-Kotta

---
+++ src/ch/uzh/phys/ecn/oboma/functions/api/RK2.java
@@ -0,0 +1,24 @@
+
+public class RK2 {
+
+    ODEEuler euler = new ODEEuler();
+
+    double[] yn1;
+    double[] temp1;
+    double[] temp2;
+    double[] stepn1 = new double[3];
+
+    public double[] step(double[] yn, double tn, double dt, DGL func){
+        yn1 = euler.step(yn, tn, dt, func);
+
+        temp1 = func.dydt(yn, dt);
+        temp2 = func.dydt(yn1, dt);
+
+        for(int i = 0; i < yn.length; i++){
+            stepn1[i] = yn[i] + dt*((temp1[i] + temp2[i])/2);
+        }
+
+        return stepn1;
+    }
+
+}
