145ce8e5badb272bdf5fc18cc0fc009297584399
wetret <retowettstein@hotmail.com>
2014-11-24 16:13:33 +0100

    Add VaccinationFunction

---
+++ src/ch/uzh/phys/ecn/oboma/functions/vaccinationfunction/VaccinationFunction.java
@@ -0,0 +1,16 @@
+package ch.uzh.phys.ecn.oboma.functions.vaccinationfunction;
+
+import ch.uzh.phys.ecn.oboma.agents.api.Agent;
+import ch.uzh.phys.ecn.oboma.common.InfectionState;
+import ch.uzh.phys.ecn.oboma.functions.api.ITransformationFunction;
+import ch.uzh.phys.ecn.oboma.map.api.INode;
+
+
+public class VaccinationFunction implements ITransformationFunction {
+
+    @Override
+    public InfectionState apply(Agent pAgent, INode pNode) {
+        return InfectionState.IMMUNE;
+    }
+
+}
