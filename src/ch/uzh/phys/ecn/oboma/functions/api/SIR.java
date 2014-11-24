2565bccd67fb142362a021d728c8ae1bf636e51a
wetret <retowettstein@hotmail.com>
2014-11-24 14:21:14 +0100

    Add SIR

---
+++ src/ch/uzh/phys/ecn/oboma/functions/api/SIR.java
@@ -0,0 +1,16 @@
+
+public class SIR implements DGL {
+
+    public static final double INFECTION_RATE = 0.00218d;
+    public static final double RECOVERY_RATE = 0.5;
+    private double[] dy = new double[3];
+
+    public double[] dydt(double[] y, double t){
+        dy[0] = -INFECTION_RATE * y[1]*y[0]; //S
+        dy[2] = RECOVERY_RATE * y[1]; //R
+        dy[1] = - dy[0] - dy[2]; //I
+
+        return dy;
+    }
+
+}
