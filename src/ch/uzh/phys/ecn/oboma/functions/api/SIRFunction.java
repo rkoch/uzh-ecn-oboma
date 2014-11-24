8b2e7f23308c5ac9572f356b651bd1ac1810b0dc
wetret <retowettstein@hotmail.com>
2014-11-24 14:22:12 +0100

    Add calculateSIR()

--- src/ch/uzh/phys/ecn/oboma/functions/api/SIRFunction.java
+++ src/ch/uzh/phys/ecn/oboma/functions/api/SIRFunction.java
@@ -4,10 +4,14 @@ import ch.uzh.phys.ecn.oboma.agents.api.Agent;
 import ch.uzh.phys.ecn.oboma.map.api.INode;
 
 
-public class TransformationFunction implements ITransformationFunction{
+public class SIRFunction implements ITransformationFunction{
 
     @Override
     public void apply(Agent pAgent, INode pNode) {
+
+    }
+
+    private void calculateSIR(){
 
     }
 
