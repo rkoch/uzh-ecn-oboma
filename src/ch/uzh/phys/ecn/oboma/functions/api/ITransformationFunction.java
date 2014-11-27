4cb09692e1e3529ddbf97569a204cf7367227f1e
Remo Koch <rko@strawberry.io>
2014-11-27 12:15:38 +0100

    Improve ITransformationFunction with comments and formatting

--- src/ch/uzh/phys/ecn/oboma/functions/api/ITransformationFunction.java
+++ src/ch/uzh/phys/ecn/oboma/functions/api/ITransformationFunction.java
@@ -1,11 +1,44 @@
+/*
+ * The MIT License (MIT)
+ * Copyright © 2014 different authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the “Software”), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package ch.uzh.phys.ecn.oboma.functions.api;
 
-import ch.uzh.phys.ecn.oboma.agents.api.Agent;
+import ch.uzh.phys.ecn.oboma.agents.model.Agent;
+import ch.uzh.phys.ecn.oboma.common.InfectionState;
 import ch.uzh.phys.ecn.oboma.map.api.INode;
 
 
 public interface ITransformationFunction {
 
-    void apply(Agent pAgent, INode pNode);
+    /**
+     * **IMPORTANT**
+     * DOES NOT MODIFY THE AGENT! THIS WILL BE DONE AFTERWARDS.
+     *
+     * If you need to store additional properties for each agent
+     * you may create a map which contains the Agent-ID (which is unique)
+     * as key and a properties object for further use. This map is
+     * in your own control, so you may change what you want there.
+     *
+     * @param pAgent
+     * @param pNode
+     * @return The new state of this agent
+     */
+    InfectionState apply(Agent pAgent, INode pNode);
 
 }
