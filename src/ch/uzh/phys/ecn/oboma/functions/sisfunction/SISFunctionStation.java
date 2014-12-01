4c58eab7c95e930199221a5921eeb0eaacbc6df5
Reto Wettstein <retowettstein@hotmail.com>
2014-12-01 16:34:41 +0100

    More changes to SIS

--- src/ch/uzh/phys/ecn/oboma/functions/sisfunction/SISFunctionStation.java
+++ src/ch/uzh/phys/ecn/oboma/functions/sisfunction/SISFunctionStation.java
@@ -5,38 +5,34 @@ import ch.uzh.phys.ecn.oboma.common.DiseaseConstants;
 import ch.uzh.phys.ecn.oboma.map.api.INode;
 
 
-public class SISFunctionStation extends SISFunction{
-    
+public class SISFunctionStation
+        extends SISFunction {
+
     @Override
-    public void onBeforeTimestep(INode pNode){
+    public void onBeforeTimestep(INode pNode) {
         mDiseaseDistributionInNode = AgentUtils.getDiseaseDistributionInNode(pNode);
         mNewDiseaseDistributionInNode = calculateSIR(mDiseaseDistributionInNode[0], mDiseaseDistributionInNode[1], mDiseaseDistributionInNode[2] + mDiseaseDistributionInNode[3]);
 
         mInfectionPercentage = getPercentage(pNode.getAllAgents().size(), Math.abs(mDiseaseDistributionInNode[0] - mNewDiseaseDistributionInNode[0]));
         mRecoveryPercentage = getPercentage(pNode.getAllAgents().size(), Math.abs(mDiseaseDistributionInNode[2] - mNewDiseaseDistributionInNode[2]));
     }
-    
-    @Override
-    public void onAfterTimestep(INode pNode){
-        
-    }
-    
-    protected double[] calculateSIR(double pSusceptible, double pInfected, double pRecovered){
+
+    protected double[] calculateSIR(double pSusceptible, double pInfected, double pRecovered) {
         ODESIR sir = new ODESIR(DiseaseConstants.STATION_INFECTION_RATE);
         ODERK2 rk2 = new ODERK2();
 
         double dt = 1;
         double tn = 1;
-        double[] yn = {pSusceptible, pInfected, pRecovered};
+        double[] yn = { pSusceptible, pInfected, pRecovered };
         double[] yn1 = new double[3];
 
         yn1 = rk2.step(yn, tn, dt, sir);
 
         return yn1;
     }
-    
-    protected double getPercentage(double pNumberOfAgents, double pNew){
-        return pNew/pNumberOfAgents;
+
+    protected double getPercentage(double pNumberOfAgents, double pNew) {
+        return pNew / pNumberOfAgents;
     }
 
 }
