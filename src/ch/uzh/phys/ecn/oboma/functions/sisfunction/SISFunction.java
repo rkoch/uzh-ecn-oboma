15095099f236f4a725385369491408962806d18a
Reto Wettstein <retowettstein@hotmail.com>
2014-12-01 16:31:33 +0100

    Changes on SISFunctions using inheritance

--- src/ch/uzh/phys/ecn/oboma/functions/sisfunction/SISFunction.java
+++ src/ch/uzh/phys/ecn/oboma/functions/sisfunction/SISFunction.java
@@ -3,34 +3,34 @@ package ch.uzh.phys.ecn.oboma.functions.sisfunction;
 import java.util.Random;
 
 import ch.uzh.phys.ecn.oboma.agents.model.Agent;
-import ch.uzh.phys.ecn.oboma.common.AgentUtils;
-import ch.uzh.phys.ecn.oboma.common.DiseaseConstants;
 import ch.uzh.phys.ecn.oboma.common.InfectionState;
 import ch.uzh.phys.ecn.oboma.functions.api.ITransformationFunction;
 import ch.uzh.phys.ecn.oboma.map.api.INode;
 
 
-public class SISFunction implements ITransformationFunction{
+public abstract class SISFunction implements ITransformationFunction{
 
-   private double[] mDiseaseDistributionInNode = new double[4];
-   private double[] mNewDiseaseDistributionInNode = new double[3];
+   protected double[] mDiseaseDistributionInNode = new double[4];
+   protected double[] mNewDiseaseDistributionInNode = new double[3];
 
-   private double mInfectionPercentage;
-   private double mRecoveryPercentage;
+   protected double mInfectionPercentage = 0.0d;
+   protected double mRecoveryPercentage = 0.0d;
 
     @Override
-    public void onBeforeTimestep(INode pNode){
-        mDiseaseDistributionInNode = AgentUtils.getDiseaseDistributionInNode(pNode);
-        mNewDiseaseDistributionInNode = calculateSIR(mDiseaseDistributionInNode[0], mDiseaseDistributionInNode[1], mDiseaseDistributionInNode[2] + mDiseaseDistributionInNode[3]);
-
-        mInfectionPercentage = getPercentage(pNode.getAllAgents().size(), (mDiseaseDistributionInNode[0] - mNewDiseaseDistributionInNode[0]));
-        mRecoveryPercentage = getPercentage(pNode.getAllAgents().size(), mDiseaseDistributionInNode[2] + mNewDiseaseDistributionInNode[2]);
-    }
+    public abstract void onBeforeTimestep(INode pNode);
     
     @Override
     public void onAfterTimestep(INode pNode){
+        mDiseaseDistributionInNode = new double[4];
+        mNewDiseaseDistributionInNode = new double[3];
         
+        mInfectionPercentage = 0.0d;
+        mRecoveryPercentage = 0.0d;
     }
+    
+    protected abstract double[] calculateSIR(double pSusceptible, double pInfected, double pRecovered);
+    
+    protected abstract double getPercentage(double pNumberOfAgents, double pNew);
 
     @Override
     public InfectionState apply(Agent pAgent, INode pNode) {
@@ -48,23 +48,6 @@ public class SISFunction implements ITransformationFunction{
         return newAgentInfectionState;
     }
 
-    private double[] calculateSIR(double pSusceptible, double pInfected, double pRecovered){
-        ODESIR sir = new ODESIR(DiseaseConstants.TRAIN_INFECTION_RATE);
-        ODERK2 rk2 = new ODERK2();
-
-        double dt = 1;
-        double tn = 1;
-        double[] yn = {pSusceptible, pInfected, pRecovered};
-        double[] yn1 = new double[3];
-
-        yn1 = rk2.step(yn, tn, dt, sir);
-
-        return yn1;
-    }
-
-    private double getPercentage(double pNumberOfAgents, double pNew){
-        return pNew/pNumberOfAgents;
-    }
 
     private InfectionState getNewInfectionState(Agent pAgent){
         InfectionState infectionState = pAgent.getState();
