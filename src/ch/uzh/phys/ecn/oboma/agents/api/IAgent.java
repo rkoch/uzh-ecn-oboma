c40a871e719ddfb7d369e898642d8b00ebd0a04f
Raphael Matile <raphael.matile@bluewin.ch>
2014-12-01 16:49:32 +0100

    Add public API

--- src/ch/uzh/phys/ecn/oboma/agents/api/IAgent.java
+++ src/ch/uzh/phys/ecn/oboma/agents/api/IAgent.java
@@ -1,5 +1,16 @@
 package ch.uzh.phys.ecn.oboma.agents.api;
 
+import ch.uzh.phys.ecn.oboma.common.InfectionState;
+
 public interface IAgent {
 
+	public String getId();
+
+	public int getPreferredTimeOfStay(String pNodeId);
+
+	public String getNextWaypoint(String pCurrentNodeId);
+
+	public InfectionState getState();
+
+	public void setState(InfectionState pState);
 }
